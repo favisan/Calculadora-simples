802646da1b7ccd396dd1275a6f44c9d305bacb3b
Treinamento <Treinamento@RD723925.raiadrogasil.com>
2020-02-07 09:19:14 -0300

    ajustando =

--- javascript/code.js
+++ javascript/code.js
@@ -1,12 +1,15 @@
-const limpar=()=>{
-    document.getElementById("visor").value="";
-}
-const preencher=valor=>{
-    document.getElementById("visor").value+=valor;
-}
-const calcular=()=>{
-    let resultado = 0;
-    resultado = document.getElementById("visor").value;
+const limpar = () => {
     document.getElementById("visor").value = "";
-    document.getElementById("visor").value= eval(resultado);
+}
+const preencher = valor => {
+    document.getElementById("visor").value += valor;
+}
+const calcular = () => {
+    if (document.getElementById("visor").value != "") {
+        let resultado = 0;
+        resultado = document.getElementById("visor").value;
+        document.getElementById("visor").value = "";
+        document.getElementById("visor").value = eval(resultado);
+    }
+    else { document.getElementById("visor").value = 0 }
 }
